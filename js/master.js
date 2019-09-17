affe2a66e2bc3e892d2d2659fb3aa1fd065fde7c
Magda-Kilonzo <51301099+Magda-Kilonzo@users.noreply.github.com>
2019-09-17 15:58:25 +0300

    Add files via upload
    Just css and html

---
+++ js/master.js
@@ -0,0 +1,9 @@
+// const welcome = document.querySelector(".welcome");
+
+// welcome.addEventListener('mouseover', () => {
+//     welcome.style = "background-color: #800000e1";
+// });
+
+// welcome.addEventListener('mouseout', () => {
+//     welcome.style = "background-color: white";
+// });
